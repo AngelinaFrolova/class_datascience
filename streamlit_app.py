dc0eef2509a4d948f55be0343ac8b0f5286124a2
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-02-22 16:28:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
 
+st.write("""
+#CROPS YIELD DATA VISUALIZATION
+
+based on rainfall, average temperature and pesticides used
+
+""")
+
+url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
+df_yield= pd.read_csv(url)
+
+st.area_chart(data=df_yield, x=Year, y=hg/ha_yield, use_container_width=True)
