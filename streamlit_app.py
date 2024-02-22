bddd94a8f9b8eab69ef60953e58731ce4621507e
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-02-22 16:31:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import pandas as pd
-import numpy as np
+import streamlit as st
 
 st.write("""
-#CROPS YIELD DATA VISUALIZATION
+# CROPS YIELD DATA VISUALIZATION
 
-based on rainfall, average temperature and pesticides used
-
+based on rainfall, average temperature, and pesticides used
 """)
 
 url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
-df_yield= pd.read_csv(url)
+df_yield = pd.read_csv(url)
 
-st.area_chart(data=df_yield, x=Year, y=hg/ha_yield, use_container_width=True)
+# Assuming 'Year' and 'hg/ha_yield' are column names in your DataFrame
+st.area_chart(data=df_yield, x='Year', y='hg/ha_yield', use_container_width=True)
+
