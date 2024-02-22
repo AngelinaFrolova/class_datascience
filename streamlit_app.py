572482649d7346b4178d2f826c1cafa767f28cea
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-02-22 16:59:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
 df_yield= pd.read_csv(url)
 
 # Assuming df_yield is your DataFrame
-data = df_yield['Item'].value_counts()
+data = df_yield['Item'].value_counts().reset_index()
 
 # Streamlit app
 st.title('Shares of crops')
 
+# Create a pie chart using Plotly Express
+fig = px.pie(data, names='index', values='Item', title='Shares of crops')
+
 # Display the pie chart using Streamlit
-st.pie_chart(data)
-
-
-# Display the pie chart using Streamlit
-st.pyplot(fig)
-
+st.plotly_chart(fig)
