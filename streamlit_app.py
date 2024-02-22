e486c0ecc9c6c2d8dc3d9cfbb64818195df28de1
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-02-22 16:40:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Assuming df_yield is your DataFrame
 data = df_yield['Item'].value_counts()
@@ -10,16 +9,9 @@ st.write("""
 # Shares of Crops
 """)
 
-# Create a matplotlib figure
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.set_title('Shares of crops')
+# Plot the pie chart directly using Streamlit's st.pyplot()
+st.pyplot(plt.pie(data.values, labels=data.index, autopct='%1.1f%%'))
 
-# Use Pastel1 colormap
-cmap = plt.get_cmap('Pastel1')
-colors = cmap(range(len(data)))
-
-# Plot the pie chart
-ax.pie(data.values, labels=data.index, colors=colors, autopct='%1.1f%%')
 
 # Display the chart using Streamlit's st.pyplot()
 st.pyplot(fig)
