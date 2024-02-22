22a05573252cf7a697c33d61275c7e527cee9ada
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-02-22 16:41:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Assuming df_yield is your DataFrame
-data = df_yield['Item'].value_counts()
+url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
+df_yield = pd.read_csv(url)
 
 # Create a Streamlit app
 st.write("""
 # Shares of Crops
 """)
 
-# Plot the pie chart directly using Streamlit's st.pyplot()
-st.pyplot(plt.pie(data.values, labels=data.index, autopct='%1.1f%%'))
-
-
-# Display the chart using Streamlit's st.pyplot()
-st.pyplot(fig)
-
-
+# Check if df_yield is not empty
+if not df_yield.empty:
+    # Plot the pie chart directly using Streamlit's st.pyplot()
+    st.pyplot(plt.pie(df_yield['Item'].value_counts().values, labels=df_yield['Item'].value_counts().index, autopct='%1.1f%%'))
+else:
+    st.write("Error: DataFrame is empty.")
