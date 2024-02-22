81ff3fafe5c02a326ed62d83f7cd0202526b2a97
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-02-22 16:50:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-
-# Assuming df_yield is your DataFrame
-url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
-df_yield = pd.read_csv(url)
 
 # Assuming df_yield is your DataFrame
 data = df_yield['Item'].value_counts()
@@ -13,12 +8,11 @@ data = df_yield['Item'].value_counts()
 st.title('Shares of crops')
 
 # Use Pastel1 colormap for better visualization
-cmap = plt.get_cmap('Pastel1')
-colors = cmap(range(len(data)))
+colors = st.get_option("theme.colorway")
 
 # Plotting the pie chart
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.pie(data.values, labels=data.index, colors=colors, autopct='%1.1f%%')
 
 # Display the pie chart using Streamlit
-st.pyplot(fig)
+st.pie(data, labels=data.index, colors=colors, autopct='%1.1f%%')
+
