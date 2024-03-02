0a628c23bfbca35843583a55ddaf16bbe85207f3
AngelinaFrolova <114063641+AngelinaFrolova@users.noreply.github.com>
2024-03-02 15:53:36 +0100

    Update streamlit_app_crops.py

--- streamlit_app_crops.py
+++ streamlit_app_crops.py
@@ -18,7 +18,7 @@ background_image = """
     background-size: 100vw 100vh;
     background-position: center;  
     background-repeat: no-repeat;
-    opacity: 0.8;
+    opacity: 0.2;
 }
 </style>
 """
